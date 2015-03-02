1815cdd1c7d424359f3d3a7ed59132f9c07f3bae
Ricardo Gomez <ricardo.gomez331@gmail.com>
2015-03-02 00:17:39 -0600

    Dispatch macros

--- app/src/main/java/com/rhg135/derpyirc/core/Core.java
+++ app/src/main/java/com/rhg135/derpyirc/core/Core.java
@@ -8,6 +8,8 @@ import com.google.common.base.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Method;
+
 /**
  * Created by rhg135 on 01/03/15.
  */
@@ -36,6 +38,20 @@ public final class Core {
             final String[] macroParts = commandLine.split("\\s+", 1);
             final String macroName = macroParts[0].substring(1);
             logger.debug("Dispatching on macro " + macroName);
+            PersistentMap loadedMacros = (PersistentMap) ((PersistentMap) globalState.getState()).get("macros");
+            if (loadedMacros == null) {
+                logger.info("No such macro " + macroName);
+                logger.debug("Loaded Macros: " + loadedMacros);
+                return;
+            } else {
+                try {
+                    final Class klass = (Class) loadedMacros.get(macroName);
+                    final Method method = klass.getDeclaredMethod("macro", PersistentMap.class, macroParts.getClass());
+                    method.invoke(null, globalState, macroParts);
+                } catch (Throwable e) {
+                    logger.error("Macro failure", e);
+                }
+            }
         }
     }
 }

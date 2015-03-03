7d103bfceaf13686d46b1608b8b5a5787c88ecd9
Ricardo Gomez <ricardo.gomez331@gmail.com>
2015-03-03 13:42:57 -0600

    wtf

--- app/src/main/java/com/rhg135/derpyirc/core/StateMacro.java
+++ app/src/main/java/com/rhg135/derpyirc/core/StateMacro.java
@@ -1,11 +1,13 @@
 package com.rhg135.derpyirc.core;
 
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Ricardo on 3/2/2015.
  */
 public class StateMacro<S> implements IMacro<S> {
     @Override
     public void macro(S globalState, String[] commandParts) {
-        Macros.logger.debug(globalState.toString());
+        LoggerFactory.getLogger(StateMacro.class).debug("State: " + globalState.toString());
     }
 }

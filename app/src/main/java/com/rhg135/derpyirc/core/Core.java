775d50b72e55ab70baa384bf0fd479f2b7597bfa
Ricardo Gomez <ricardo.gomez331@gmail.com>
2015-03-03 14:44:34 -0600

    because i'm lazy commands are macros with full split on spaces for now

--- app/src/main/java/com/rhg135/derpyirc/core/Core.java
+++ app/src/main/java/com/rhg135/derpyirc/core/Core.java
@@ -1,14 +1,9 @@
 package com.rhg135.derpyirc.core;
 
 import com.github.krukow.clj_ds.PersistentMap;
-import com.github.krukow.clj_ds.PersistentVector;
-import com.github.krukow.clj_ds.Persistents;
-import com.google.common.base.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Method;
 
 /**
  * Created by rhg135 on 01/03/15.
@@ -24,6 +19,25 @@ public final class Core {
             final String[] macroParts = commandLine.split("\\s+", 1);
             final String macroName = macroParts[0].substring(1);
             logger.debug("Dispatching on macro " + macroName);
+            PersistentMap<String, IMacro<PersistentMap>> loadedMacros = (PersistentMap<String, IMacro<PersistentMap>>) globalState.getState().get("macros");
+            if (loadedMacros == null) {
+                logger.info("No Loaded Macros");
+            } else {
+                try {
+                    final IMacro<PersistentMap> macro = loadedMacros.get(macroName);
+                    if (macro == null) {
+                        // TODO: don't use exceptions for flow control
+                        throw new RuntimeException("No such macro");
+                    }
+                    macro.macro(globalState.getState(), macroParts);
+                } catch (Throwable e) {
+                    logger.error("Macro failure", e);
+                }
+            }
+        } else if (commandLine.startsWith("/")) {
+            final String[] macroParts = commandLine.split("\\s+");
+            final String macroName = macroParts[0].substring(1);
+            logger.debug("Dispatching on command " + macroName);
             PersistentMap<String, IMacro<PersistentMap>> loadedMacros = (PersistentMap<String, IMacro<PersistentMap>>) globalState.getState().get("macros");
             if (loadedMacros == null) {
                 logger.info("No Loaded Macros");
